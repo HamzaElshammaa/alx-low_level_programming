8c5f93c7014ee2b634aa7af1c47c4dba2c50d89b
Hamza <mozamiza@gmail.com>
2023-09-07 14:12:31 -0700

    0x03

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,12 @@
 #include <stdio.h>
+/**
+ * main -Entry point
+ *
+ * Description: puts phrase
+ * @parameter: no parameters
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
