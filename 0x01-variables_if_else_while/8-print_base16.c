a529f8fead43b6a05eada8904ceb0f9f1e3ed60f
Hamza <mozamiza@gmail.com>
2023-09-11 03:15:54 -0700

    0x03

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char start = '0';
+char end = 'F';
+char ch;
+for (ch = start; ch <= end; ch++)
+{
+putchar(ch);
+}
+putchar('\n');
+return (0);
+}
