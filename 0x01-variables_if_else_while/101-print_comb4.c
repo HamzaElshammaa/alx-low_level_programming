b258d9b5888594e1b76512066eddc970f70b88fb
Hamza <mozamiza@gmail.com>
2023-09-11 04:20:44 -0700

    0x03

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int ch;
+int ch1;
+int ch2 = 1;
+for (ch1 = 0; ch1 <= 8; ch1++)
+{
+for (ch = ch2; ch <= 9; ch++)
+{
+putchar('0' + ch1);
+putchar('0' + ch);
+if (ch1 != 8 && ch2 != 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+ch2++;
+}
+putchar('\n');
+return (0);
+}
