f4498b56d8c596d781884e521cd6c42024fe8b29
Hamza <mozamiza@gmail.com>
2023-09-11 04:53:11 -0700

    0x03

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -21,7 +21,7 @@ for (units = ustart; units <= 9; units++)
 putchar('0' + hunds);
 putchar('0' + tens);
 putchar('0' + units);
-if (hunds != 7 && tens != 8 && units != 9)
+if (hunds != 7 && tens != 8)
 {
 putchar(',');
 putchar(' ');
