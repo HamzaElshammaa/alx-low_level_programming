6034b8b8bf1ab43d8feb973599b9401efa05bdf3
Hamza <mozamiza@gmail.com>
2023-09-14 03:02:08 -0700

    0x03

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * print_alphabet - check the code
+ * print_alphabet_x10 - check the code
  *
  * Return: Always 0.
  */
@@ -16,7 +16,7 @@ for (letter = 'a'; letter <= 'z'; letter++)
 {
 putchar(letter);
 }
-}
 putchar('\n');
+}
 }
 
