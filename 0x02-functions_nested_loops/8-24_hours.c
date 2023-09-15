16be668d137214fcf005dba908da8eec3f57107f
Hamza <mozamiza@gmail.com>
2023-09-15 07:35:36 -0700

    0x03

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * jack_bauer - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+void jack_bauer(void)
+{
+	int hour;
+	int minute;
+
+	for (hour = 0; hour < 24; hour++)
+	{
+		for (minute = 0; minute < 60; minute++)
+		{
+			char hour_str[3], minute_str[3];
+
+			snprintf(hour_str, sizeof(hour_str), "%02d", hour);
+			snprintf(minute_str, sizeof(minute_str), "%02d", minute);
+
+			printf("%s:%s\n", hour_str, minute_str);
+		}
+	}
+
+}
