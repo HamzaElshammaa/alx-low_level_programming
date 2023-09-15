d576d18d1937da2d400d35dfdbd1165081ee54d5
Hamza <mozamiza@gmail.com>
2023-09-15 14:43:12 -0700

    0x03

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * _isupper - check the code
+ *
+ * @c: the num to check
+ * Return: Always 0.
+ */
+int _isupper(int c)
+{
+if (isupper(c))
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
