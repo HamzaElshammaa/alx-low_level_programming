426df7340cb0396d038859bd598b532ee59d7aca
Hamza <mozamiza@gmail.com>
2023-09-15 07:28:28 -0700

    0x03

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,18 +1,31 @@
-#include <stdio.h>
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * print_last_digit - check code
- * @n: number to get digit of
- * Return: value of last digit
+ * print_last_digit - Entry point
+ *
+ * @n: value
+ *
+ * Return: Always 0 (Success)
  */
+
 int print_last_digit(int n)
-int digit;
 {
-if (n < 0)
-{
-	n = n * -1;
-}
-digit = n % 10;
-return (digit);
+	int last_digit;
+
+	if (n < 0)
+	{
+		n = n * -1;
+	}
+
+	last_digit = n % 10;
+	if (last_digit < 0)
+	{
+		last_digit = last_digit * -1;
+	}
+
+	_putchar('0' + last_digit);
+
+	return (last_digit);
 }
