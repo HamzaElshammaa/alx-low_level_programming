37cdd87c56d24d6baeedcc7ca8da928f41567b9d
Hamza <mozamiza@gmail.com>
2023-09-15 03:00:23 -0700

    0x03

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * _abs - check the code
+ * @n: num to abs
+ * Return: returns abs value
+ */
+int _abs(int n)
+{
+int res = abs(n);
+return (res);
+}
