e480475fd76b9b8116089ef32751f06c6773b018
Hamza <mozamiza@gmail.com>
2023-09-15 01:54:10 -0700

    0x03

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _islower - check the code
+ *
+ * @c: the num to check
+ * Return: Always 0.
+ */
+int _islower(int c)
+{
+if (islower(c))
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,4 +2,5 @@
 #define HEADER_FILE
 void print_alphabet(void);
 void print_alphabet_x10(void);
+int _islower(int c);
 #endif
