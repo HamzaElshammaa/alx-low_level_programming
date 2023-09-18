40da7a1223ba3a355eff3383a9cfecc2325a78e4
Hamza <mozamiza@gmail.com>
2023-09-18 13:38:34 -0700

    0x03

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,7 +10,7 @@ void print_rev(char *s)
 {
 int i;
 int len = strlen(s);
-char rev[len + 1];
+char rev = (char *)malloc((len + 1)* sizeof(char));
 for (i = 0; i < len; i++)
 {
 rev[i] = s[len - 1 - i];
