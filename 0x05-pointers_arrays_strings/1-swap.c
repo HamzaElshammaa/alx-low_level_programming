418d020feac560c0787a65709c285809ac85cb3a
Hamza <mozamiza@gmail.com>
2023-09-18 11:45:12 -0700

    0x03

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * swap_int - Swaps ints
+ * @a: int a
+ * @b: int b
+ * Return: always 0
+ */
+void swap_int(int *a, int *b)
+{
+temp = *a;
+a = *b;
+b = temp;
+return;
+}
