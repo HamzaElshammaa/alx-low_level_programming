87aba387226b1198e8d712e2ae6ced192eb420d5
Hamza <mozamiza@gmail.com>
2023-09-18 13:51:52 -0700

    0x03

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -12,9 +12,9 @@ void print_rev(char *s)
 int i;
 int len = strlen(s);
 char *tmp = s + len - 1;
-for (i = (len - 1); i > 0; i--)
+for (i = len; i > 0; i--)
 {
-putchar(tmp[i]);
+putchar(*tmp[i]);
 }
 putchar('\n');
 return;
