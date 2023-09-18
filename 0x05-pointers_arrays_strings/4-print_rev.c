64e787c377e3a8b26d45100ad722c44a56e78c7f
Hamza <mozamiza@gmail.com>
2023-09-18 15:01:57 -0700

    0x03

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,10 +9,8 @@
 
 void print_rev(char *s)
 {
-        char rev = s[0];
         int counter = 0;
         int i;
-
         while (s[counter] != '\0')
         counter++;
         for (i = counter; i >= 0; i--)
