1dd4ba887442d8de3ad2eca411fd3317d8ba0c73
Hamza <mozamiza@gmail.com>
2023-09-22 02:30:57 -0700

    0x06

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+/**
+ * _strcmp - check code
+ * @s1: check code
+ * @s2: check code
+ * Return: int
+ */
+int _strcmp(char *s1, char *s2)
+{
+int result = strcmp(s1, s2);
+return (result);
+}
