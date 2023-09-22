b9d0ce8b52c42f63c3b55e19fdfb7bf69793cb69
Hamza <mozamiza@gmail.com>
2023-09-22 02:10:16 -0700

    0x03

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+/**
+ * _strcat - Check code
+ * @dest : var one
+ * @src : var two
+ * Return: always 0
+ */
+char *_strcat(char *dest, char *src)
+{
+strcat(dest, src);
+return (dest);
+}
