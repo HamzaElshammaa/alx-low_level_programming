a2a6dd0d4fcda604837f40be0359ec9959cd118d
Hamza <mozamiza@gmail.com>
2023-09-26 02:52:11 -0700

    0x06

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,26 +1,21 @@
-#include <stdio.h>
 #include "main.h"
 /**
- * reverse_array - check code
- * @a: variable one
- * @n: number variable
- * Return: always 0
+ * reverse_array - Check the code
+ * @a: var
+ * @n: var
+ * Return: nohig
  */
 void reverse_array(int *a, int n)
 {
+int *end = a + n - 1;
 int tmp;
-int *pntr;
-pntr = a;
 int i;
-for (i = 0; i < n; i++)
-a++;
-for (i = 0; i < n; i++)
+for (i = 0; i < end; i++)
 {
-tmp = *pntr;
-*pntr = *a;
-*a = tmp;
-a--;
-pntr++;
+tmp = *a[i];
+*a[i] = *a[end];
+*a[end] = tmp;
+end--;
 }
 return;
 }
