c8d5926cef28a0f0213fc33eec8004b1b8694b9f
Hamza <mozamiza@gmail.com>
2023-09-26 04:35:58 -0700

    0x06

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -6,6 +6,42 @@
  */
 char *cap_string(char *str)
 {
-char *start = str;
+int i;
+i = 0;
+while (str[i] != '\0')
+{
+if (i == 0)
+str[i] = str[i] - 32;
+switch (str[i])
+{
+case ' ':
+	str[i] = ' ';
+case '\n':
+	str[i] = ' ';
+case ',':
+	str[i] = ' ';
+case ';':
+	str[i] = ' ';
+case '.':
+	str[i] = ' ';
+case '!':
+	str[i] = ' ';
+case '?':
+	str[i] = ' ';
+case '"':
+	str[i] = ' ';
+case '(':
+	str[i] = ' ';
+case ')':
+	str[i] = ' ';
+case '{':
+	str[i] = ' ';
+case '}':
+	str[i] = ' ';
+default:
+	str[i] = str[i] - 32;
+}
+i++;
+}
 return (str);
 }
