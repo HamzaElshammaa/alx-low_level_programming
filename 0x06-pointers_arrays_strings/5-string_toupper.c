362b64b6be102ce7e74e6eb4f5565608d1dd0c16
Hamza <mozamiza@gmail.com>
2023-09-26 03:10:17 -0700

    0x06

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * string_toupper - check code
+ * @c: cahr pntr
+ * Return: char pointer
+ */
+char *string_toupper(char *c)
+{
+char *start = c;
+char tmp;
+while (*c != '\0')
+{
+tmp = *c;
+toupper(tmp);
+*c = tmp;
+c++;
+}
+return (start);
+}
