d2710f4d8bc680f73d8c7a5805834788adbfd67f
Hamza <mozamiza@gmail.com>
2023-10-04 05:53:27 -0700

    0x06

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,21 +1,20 @@
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
-/* more headers goes there */
+#include "main.h"
 
 /**
  * _isupper - Entry point
- *@c: parameter
+ * @c: the character to be checker
  * Return: Always 0 (Success)
  */
 int _isupper(int c)
 {
-if (c >= 'A' && c <= 'Z')
-{
-return (1);
-}
-else
-{
-return (0);
-}
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+
+
 }
