f33fd37f276927b8bed5030adf0d319d1084ad3e
Hamza <mozamiza@gmail.com>
2023-10-04 05:55:47 -0700

    0x06

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - Entry point
+ * @a: the character to be checker
+ * @b: the second character
+ * Return: Always 0 (Success)
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+
+}
