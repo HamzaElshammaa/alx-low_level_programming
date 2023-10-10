b1a2d63b1e623de17a581da582774f6b3aa80603
Hamza <mozamiza@gmail.com>
2023-10-10 02:46:19 -0700

    0x06

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _print_rev_recursion - check the code
+ * @s: string pntr
+ * Return: nothibg
+ */
+void _print_rev_recursion(char *s)
+{
+if (*s == '\0')
+{
+_puts_recursion(s);
+}
+_print_rev_recursion(++s);
+return;
+}
