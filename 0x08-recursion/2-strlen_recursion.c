af4ea7be6dd1b41b205ff0911ffeabc59dcea1c3
Hamza <mozamiza@gmail.com>
2023-10-10 03:20:15 -0700

    0x06

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strlen_recursion - check code
+ * @s: string pnt
+ * Return: int
+ */
+int _strlen_recursion(char *s)
+{
+return (strlen(s));
+}
