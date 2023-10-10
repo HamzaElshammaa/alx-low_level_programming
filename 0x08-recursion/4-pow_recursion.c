f25b39462c7e232eb5500f6a9acd41c04b687706
Hamza <mozamiza@gmail.com>
2023-10-10 03:33:02 -0700

    0x06

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+#include <math.h>
+/**
+ * _pow_recursion - check code
+ * @x: int
+ * @y: int
+ * Return: and int
+ */
+int _pow_recursion(int x, int y)
+{
+if (y < 0)
+{
+return (-1);
+}
+return (pow(x, y));
