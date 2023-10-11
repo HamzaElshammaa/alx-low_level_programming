9914d99465c35bf81bf362eac1cd9401acb3fac1
Hamza <mozamiza@gmail.com>
2023-10-11 06:37:41 -0700

    0x06

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -12,6 +12,7 @@ int **alloc_grid(int width, int height)
 {
 int **arrayrows;
 int i;
+int j;
 if (width <= 0 || height <= 0)
 	return (NULL);
 arrayrows = malloc(sizeof(int) * width);
@@ -22,6 +23,12 @@ for (i = 0; i < width; i++)
 arrayrows[i] = malloc(sizeof(int) * height);
 if (arrayrows[i] == NULL)
 return (NULL);
+}
+for (i = 0; i < width; i++)
+{
+for (j = 0; j < heigth; j++)
+{
+arrayrows[i][j] = 0;
 }
 return (arrayrows);
 }
