4036a853e02e54a5abcd4370ae68d40b3c03b445
Hamza <mozamiza@gmail.com>
2023-10-11 06:02:01 -0700

    0x06

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * _strdup - check code
+ * @str: string pointer
+ * Return: a char pointer
+ */
+char *_strdup(char *str)
+{
+int i;
+char *dup;
+int size;
+if (str == NULL)
+return (NULL);
+size = strlen(str);
+dup = malloc(sizeof(char) * size);
+for (i = 0; i < size; i++)
+{
+dup[i] = str[i];
+}
+return (dup);
+}
