650b44c507cf2368cb7567c6bff66b3ebec61cfc
Hamza <mozamiza@gmail.com>
2023-10-11 05:48:24 -0700

    0x06

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -9,12 +9,14 @@
  */
 char *create_array(unsigned int size, char c)
 {
+unsigned int i;
+char *array;
 if (size == 0)
 return (NULL);
-char *array = malloc(sizeof(char) * size);
+array = malloc(sizeof(char) * size);
 if (array == NULL)
 	return (NULL);
-for (int i = 0; i < size; i++)
+for (i = 0; i < size; i++)
 {
 	array[i] = c;
 }
