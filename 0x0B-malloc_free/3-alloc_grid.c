1d5de46483c970d2e3e3297b0b0824c673b29bb3
Hamza <mozamiza@gmail.com>
2023-10-11 06:34:37 -0700

    0x06

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * alloc_grid - chekc code
+ * @width: width
+ * @height: height
+ * Return: something
+ */
+int **alloc_grid(int width, int height)
+{
+int **arrayrows;
+int i;
+if (width <= 0 || height <= 0)
+	return (NULL);
+arrayrows = malloc(sizeof(int) * width);
+if (arrayrows == NULL)
+	return (NULL);
+for (i = 0; i < width; i++)
+{
+arrayrows[i] = malloc(sizeof(int) * height);
+if (arrayrows[i] == NULL)
+return (NULL);
+}
+return (arrayrows);
+}
+
