72fa28456656767fd72839ed90100a26f388a56a
Hamza <mozamiza@gmail.com>
2023-10-13 13:50:27 -0700

    0x06

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,5 @@
-#ifndef MYHEADER
-#define MYHEADER
+#ifndef MYHDR
+#define MYHDR
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
